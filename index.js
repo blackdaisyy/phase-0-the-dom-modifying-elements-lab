00e4da0fa6a591c044f34c188528caa484ee13e7
Peace Kuria <ephraimekuria@gmail.com>
2025-09-20 16:51:55 +0300

    modify

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+// 1. Remove the existing main element
+const main = document.getElementById("main");
+main.remove();
+
+// 2. Create a new h1 element
+const newHeader = document.createElement("h1");
+
+// 3. Set id
+newHeader.id = "victory";
+
+// 4. Set text content
+newHeader.textContent = "Ada Lovelace is the champion"; // Replace with your name
+
+// 5. Append to the DOM
+document.body.append(newHeader);
 // Write your code here!
